654fef66f8109a754394c6f55e207d4f2c9664b5
sergio-nnz <sergio.nnsa@gmail.com>
2021-03-13 12:47:36 -0800

    vector-lib: Add utility functions to vector.h

--- code/src/vector3.h
+++ code/src/vector3.h
@@ -2,6 +2,7 @@
 
 #ifndef VECTOR3_H
 #define VECTOR3_H
+#include <ostream>
 
 class vector3 {
 public:
@@ -28,5 +29,49 @@ public:
 
 using point3 = vector3;
 using color = vector3;
+
+inline std::ostream& operator<<(std::ostream& out, const vector3& v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vector3 operator+(const vector3& u, const vector3& v) {
+	return vector3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vector3 operator-(const vector3& u, const vector3& v) {
+	return vector3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vector3 operator*(const vector3& u, const vector3& v) {
+	return vector3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vector3 operator*(double t, const vector3& v) {
+	return vector3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vector3 operator*(vector3& v, double t) {
+	return t * v;
+}
+
+inline vector3 operator/(vector3 v, double t) {
+	return (1 / t) * v;
+}
+
+inline double dot(const vector3& u, const vector3& v) {
+	return (u.e[0] * v.e[0]) + (u.e[1] * v.e[1]) + (u.e[2] * v.e[2]);
+}
+
+inline vector3 cross(const vector3& u, const vector3& v)
+{
+	return vector3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+		           u.e[2] * v.e[0] - u.e[0] * v.e[2], 
+		           u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vector3 unit_vector(vector3 v)
+{
+	return v / v.length();
+}
 
 #endif
