31b3b6775f1d6e54f8e9ca45a8803a9f02dde9f1
sergio-nnz <sergio.nnsa@gmail.com>
2021-03-16 21:20:52 -0700

    color-utility: Make color utility method inline

--- code/include/color.h
+++ code/include/color.h
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-void WriteColor(std::ostream &out, Color pixel_color) {
+inline void WriteColor(std::ostream &out, Color pixel_color) {
 	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
