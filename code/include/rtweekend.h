0c8e23534e1aa35eddee57ab671f461d727348a4
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-03-27 18:11:57 +0100

    main-header-file: Convert degrees to radians.

--- code/include/rtweekend.h
+++ code/include/rtweekend.h
@@ -3,3 +3,9 @@
 // Constants
 
 const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
--- code/tests/test_rtweekend.cpp
+++ code/tests/test_rtweekend.cpp
@@ -4,3 +4,8 @@
 TEST(TestUtils, TestPi) {
     ASSERT_EQ(pi, 3.1415926535897932385);
 }
+
+TEST(TestUtils, TestDegreesToRadians) {
+    double test_value = 180 / pi;
+    ASSERT_DOUBLE_EQ(degrees_to_radians(test_value), 1);
+}
