56e4a68559ad55506d533df82a13f523ffcfc105
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-03-27 19:00:51 +0100

    main-header-file: Produce random numbers.

--- code/include/rtweekend.h
+++ code/include/rtweekend.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -21,7 +22,19 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0, 1)
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max - min) * random_double();
+}
+
 // Common headers
 
 #include "ray.h"
-#include "vec3.h"
+#include "vector3.h"
--- code/tests/test_rtweekend.cpp
+++ code/tests/test_rtweekend.cpp
@@ -9,3 +9,19 @@ TEST(TestUtils, TestDegreesToRadians) {
     double test_value = 180 / pi;
     ASSERT_DOUBLE_EQ(degrees_to_radians(test_value), 1);
 }
+
+TEST(TestUtils, TestRandomDoubleZeroToOne) {
+    // Verify that random_double returns a number greater or equal to zero
+    ASSERT_GE(random_double(), 0);
+    // Verify that random_double returns a number smaller than one
+    ASSERT_LT(random_double(), 1);
+}
+
+TEST(TestUtils, TestRandomDoubleMinToMax) {
+    double min = 10;
+    double max = 20;
+    // Verify that random_double returns a number greater or equal to min
+    ASSERT_GE(random_double(min, max), min);
+    // Verify that random_double returns a number smaller than max
+    ASSERT_LT(random_double(min, max), max);
+}
