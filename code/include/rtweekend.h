19a8f7747b7536f6b71e1f1910eccbd17f18ee13
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-04-02 19:25:06 +0200

    Main header file (#14)
    * main-header-file: Create unit test for Pi value.
    
    * main-header-file: Define Pi.
    
    * main-header-file: Convert degrees to radians.
    
    * main-header-file: Add extra utilities.
    
    * main-header-file: Produce random numbers.
    
    * main-header-file: Clamp value to a range.
    
    * main-header-file: Use CamelCase for function names.

---
+++ code/include/rtweekend.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <random>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double DegreesToRadians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double RandomDouble() {
+    // Returns a random real in [0, 1)
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double RandomDouble(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max - min) * RandomDouble();
+}
+
+inline double Clamp(double x, double min, double max) {
+    if (x < min) {
+        return min;
+    } else if (x > max) {
+        return max;
+    } else {
+        return x;
+    }
+}
+
+// Common headers
+
+#include "ray.h"
+#include "vector3.h"
---
+++ code/tests/test_rtweekend.cpp
@@ -0,0 +1,48 @@
+#include <rtweekend.h>
+#include <gtest/gtest.h>
+
+TEST(TestUtils, TestPi) {
+    ASSERT_EQ(pi, 3.1415926535897932385);
+}
+
+TEST(TestUtils, TestDegreesToRadians) {
+    double test_value = 180 / pi;
+    ASSERT_DOUBLE_EQ(DegreesToRadians(test_value), 1);
+}
+
+TEST(TestUtils, TestRandomDoubleZeroToOne) {
+    // Verify that RandomDouble returns a number greater or equal to zero
+    ASSERT_GE(RandomDouble(), 0);
+    // Verify that RandomDouble returns a number smaller than one
+    ASSERT_LT(RandomDouble(), 1);
+}
+
+TEST(TestUtils, TestRandomDoubleMinToMax) {
+    double min = 10;
+    double max = 20;
+    // Verify that RandomDouble returns a number greater or equal to min
+    ASSERT_GE(RandomDouble(min, max), min);
+    // Verify that RandomDouble returns a number smaller than max
+    ASSERT_LT(RandomDouble(min, max), max);
+}
+
+TEST(TestUtils, TestClampMin) {
+    double x = 5;
+    double min = 10;
+    double max = 20;
+    ASSERT_EQ(Clamp(x, min, max), min);
+}
+
+TEST(TestUtils, TestClampMax) {
+    double x = 25;
+    double min = 10;
+    double max = 20;
+    ASSERT_EQ(Clamp(x, min, max), max);
+}
+
+TEST(TestUtils, TestClampBetween) {
+    double x = 15;
+    double min = 10;
+    double max = 20;
+    ASSERT_EQ(Clamp(x, min, max), x);
+}
