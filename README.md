77a3b8432fca508be5444c6d192f6d58694e035d
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-04-04 02:25:03 +0200

    Automated tests (#17)
    * automated-tests: Rename main file for unit tests.
    
    * automated-tests: Create action for build tests.
    
    * automated-tests: Fix indentation in main.yml.
    
    * automated-tests: Name main workflow.
    
    * automated-tests: Add a workflow badge
    
    * automated-tests: Check if the executable runs from start to end.
    
    * automated-tests: Catch run failures.
    
    * automated-tests: Fix path to executable.
    
    * automated-tests: Accept escape sequence.
    
    * automated-tests: Build unit tests.
    
    * automated-tests: Try to fix the line break.
    
    * automated-tests: Try to fix the line break.
    
    * automated-tests: Separate early exit into multiple steps.
    
    * automated-tests: Rename stuff to encompass build and unit tests.
    
    * automated-tests: Rename steps.
    
    * automated-tests: Run unit tests.
    
    * automated-tests: Prepare to merge with master.
    
    Co-authored-by: Adrian <sergio.nnsa@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Bootleg Ray Tracer
+![Tests](https://github.com/MooreMachine/bootleg-raytracer/workflows/Tests/badge.svg)
+
 A bootleg ray tracing project by a bunch of mexicans.
 
 ### About
---
+++ execute_tests.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+VERSION=$(cat README.md | grep "Version" | awk '{ print $2 }')
+
+if [ -z $VERSION ]
+then
+    echo "Please make sure your README.md file has a Version number."
+    exit 1
+fi
+
+echo "Running new container..."
+
+CONTAINER_ID=$(docker run \
+    --name build_test \
+    --rm \
+    -it \
+    -d \
+    -v $(pwd)/code:/home \
+    mooremachine/easyubuntu:$VERSION)
+
+echo "Building app..."
+
+docker exec \
+    -w '/home' \
+    build_test \
+    sh -c './build_app.sh; echo "::set-output name=build_result::$?"'
+
+echo "Running app..."
+
+docker exec \
+    -w '/home' \
+    build_test \
+    sh -c './build/raytracer; echo "::set-output name=quick_run_result::$?"'
+
+echo "Building unit tests..."
+
+docker exec \
+    -w '/home' \
+    build_test \
+    sh -c './build_unit_tests.sh; echo "::set-output name=build_unit_tests_result::$?"'
+
+echo "Running unit tests..."
+
+docker exec \
+    -w '/home' \
+    build_test \
+    sh -c './build/raytracer_tests; echo "::set-output name=run_unit_tests_result::$?"'
+
+echo "Stopping container..."
+
+docker container stop -t 0 $CONTAINER_ID
