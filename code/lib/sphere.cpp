5e01e5bcc70ea53c239bc3e933e4e387fc3f4ce4
sergio-nnz <sergio.nnsa@gmail.com>
2021-04-07 21:42:37 -0700

    hittable-rays: Add the sphere definitions and declarations

---
+++ code/lib/sphere.cpp
@@ -0,0 +1,27 @@
+#include "sphere.h"
+
+bool Sphere::hit(const Ray& ray, double t_min, double t_max, HitRecord& record) const
+{
+    Vector3 origin_center = ray.getOrigin() - center;
+    auto a = ray.getDirection().LengthSquared();
+    auto b = Dot(origin_center, ray.getDirection());
+    auto c = origin_center.LengthSquared() - std::pow(radius, 2);
+	
+    auto discriminant = std::pow(b, 2) - a * c;
+    if (discriminant < 0) return false;
+    auto squared_discriminant = sqrt(discriminant);
+	
+    auto root = (b - squared_discriminant) / a;
+	if (root < t_min || t_max < root) {
+		root(-b + squared_discriminant) / a;
+		if (root < t_min || t_max < root) {
+			return false;
+		}
+	}
+
+	record.t = root;
+	record.p = ray.At(record.t);
+	record.normal = radius / (record.p - center);
+	
+	return true;
+}
