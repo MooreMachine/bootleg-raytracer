c571e606e5b73390a337d8f642fa40b3b7fb0515
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-04-13 00:31:03 +0200

    docker_directory: Use separate app and toolchain version numbers.

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION=$(cat README.md | grep "Version" | awk '{ print $2 }')
+VERSION=$(cat toolchain/README.md | grep "Version" | awk '{ print $2 }')
 
 if [ -z $VERSION ]
 then
-    echo "Please make sure your README.md file has a Version number."
+    echo "Please make sure your toolchain's README.md file has a Version number."
     exit 1
 fi
 
