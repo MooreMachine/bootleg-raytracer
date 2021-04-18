37a74f3b72565d37e5deab03af46dcc9747b0885
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-04-18 21:06:18 +0200

    30_code_cleanup: Use `camelCase` for getters and setters.

--- code/include/hitrecord.h
+++ code/include/hitrecord.h
@@ -9,7 +9,7 @@ struct HitRecord
 	double t;
 	bool front_face;
 
-	inline void set_face_normal(const Ray& ray, const Vector3& outward_normal) {
+	inline void setFaceNormal(const Ray& ray, const Vector3& outward_normal) {
 		front_face = Dot(ray.getDirection(), outward_normal) < 0;
 		normal = front_face ? outward_normal : -outward_normal;
 	}
