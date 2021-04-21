92c2859fe5d5eea530ef0bd9f2cf7db7f69bf028
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-04-21 22:47:21 +0200

    diffuse_materials: Use generic comparison matchers.

--- code/tests/unittests/test_vector3.cpp
+++ code/tests/unittests/test_vector3.cpp
@@ -7,11 +7,7 @@
 
 using namespace testing;
 
-MATCHER_P2(IsBetweenLeftClosedRange, a, b, std::string(negation ? "isn't" : "is") + " in the interval [" + PrintToString(a) + ", " + PrintToString(b) + ")") {
-    return a <= arg && arg < b;
-}
-
 TEST(TestVector3, RandomWhenNoParametersComponentIsMoreThanZeroUpToOOne) {
     auto result = Vector3::Random();
-    ASSERT_THAT(result.e, Each(IsBetweenLeftClosedRange(0, 1)));
+    ASSERT_THAT(result.e, Each(AllOf(Ge(0), Lt(1))));
 }
