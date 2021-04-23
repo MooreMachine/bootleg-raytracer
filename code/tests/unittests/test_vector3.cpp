4cc9ec817ff7bc55919d65ba7667a2cc84cf548e
sergio-nnz <sergio.nnsa@gmail.com>
2021-04-23 09:00:00 -0700

    vector3_unit_tests: Add the multiply operator tests

--- code/tests/unittests/test_vector3.cpp
+++ code/tests/unittests/test_vector3.cpp
@@ -9,43 +9,64 @@ protected:
     Vector3 test_vector;
 
     void SetUp() override {
-        test_vector = Vector3(1, 1, 1);
+        test_vector = Vector3(1.0, 1.0, 1.0);
     }
 };
 
-TEST_F(Vector3Test, MinusOperatorOperatorWhenNoParams3ReturnsInvertedVector3)
-{
-    Vector3 expectedVector(-1, -1, -1);
+TEST_F(Vector3Test, MinusOperatorOperatorWhenNoParamsReturnsInvertedVector3) {
+    Vector3 expectedVector(-1.0, -1.0, -1.0);
 
     Vector3 actualVector = -test_vector;
 
     ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
 }
 
-TEST_F(Vector3Test, MinusOperatorWhenSubstractingTwoVector3ReturnsTheSubstractionOfBothValues)
-{
-    Vector3 expectedVector(0, 0, 0);
+TEST_F(Vector3Test, MinusOperatorWhenSubstractingTwoVector3ReturnsTheSubstractionOfBothValues) {
+    Vector3 expectedVector(0.0, 0.0, 0.0);
 
     Vector3 actualVector = test_vector-test_vector;
 
     ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
 }
 
-TEST_F(Vector3Test, PlusOperatorWhenAddingUpTwoVector3ReturnsTheSumOfBothValues)
-{
-    Vector3 expectedVector(2, 2, 2);
+TEST_F(Vector3Test, PlusOperatorWhenAddingUpTwoVector3ReturnsTheSumOfBothValues) {
+    Vector3 expectedVector(2.0, 2.0, 2.0);
 
     Vector3 actualVector = test_vector + test_vector;
 
     ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
 }
 
-TEST_F(Vector3Test, PlusEqualOperatorWhenAddingUpAVector3ReturnsTheSumOfBothValues)
-{
-    Vector3 expectedVector(2, 2, 2);
+TEST_F(Vector3Test, PlusEqualOperatorWhenAddingUpAVector3ReturnsTheSumOfBothValues) {
+    Vector3 expectedVector(2.0, 2.0, 2.0);
     Vector3 actualVector = test_vector;
 
     actualVector += test_vector;
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
+}
+
+TEST_F(Vector3Test, MultiplyOperatorWhenMultiplyingTwoVector3ReturnsMultiplicationOfBothValues) {
+    Vector3 expectedVector(2.0, 2.0, 2.0);
+
+    Vector3 actualVector = test_vector * Vector3(2.0, 2.0, 2.0);
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
+}
+
+TEST_F(Vector3Test, MultiplyOperatorWhenMultiplyingADoubleAndVector3ReturnsMultiplicationOfBothValues) {
+    Vector3 expectedVector(2.0, 2.0, 2.0);
+
+    Vector3 actualVector = 2.0 * test_vector;
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
+}
+
+TEST_F(Vector3Test, MultiplyEqualOperatorWhenMultiplyingADoubleReturnsMultiplicationOfBothValues) {
+    Vector3 expectedVector(2.0, 2.0, 2.0);
+    Vector3 actualVector = test_vector;
+
+    actualVector *= 2.0;
 
     ASSERT_THAT(actualVector.e, ElementsAreArray(expectedVector.e));
 }
