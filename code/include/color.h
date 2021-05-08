9dc980f46c3717a7a713d99ec9b071c0b417cdb5
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-05-09 01:36:33 +0200

    36_more_test_coverage: Use more descriptive iterator name.

--- code/include/color.h
+++ code/include/color.h
@@ -8,15 +8,15 @@
 void GammaCorrection(Color& pixel_color, int samples_per_pixel) {
     // Divide the color by the number of samples and gamma-correct for gamma=2.0.
     auto scale = 1.0 / samples_per_pixel;
-    for (auto& i : pixel_color.e) {
-        i = sqrt(scale * i);
+    for (auto& color_channel : pixel_color.e) {
+        color_channel = sqrt(scale * color_channel);
     }
 }
 
 void To8BitColor(Color& pixel_color) {
     // Translate to [0, 255] values for each color component
-    for (auto& i : pixel_color.e) {
-        i = 256 * Clamp(i, 0.0, 0.999);
+    for (auto& color_channel : pixel_color.e) {
+        color_channel = 256 * Clamp(color_channel, 0.0, 0.999);
     }
 }
 
