3934c1fc8b261928ba49103869cc3f094a5f71b3
Adrian <sergio.nnsa@gmail.com>
2021-05-08 10:43:44 -0700

    Vector3 unit tests (#37)
    * vector3_unit_tests: Add the unit tests cpp file to our cmake list
    
    * vector3_unit_tests: Add the Vector3 test fixture
    
    * vector3_unit_tests: Add testing for the minus and plus sign operators
    
    * vector3_unit_tests: Add the multiply operator tests
    
    * vector_3_unit_tests: Add missing tests of the vector3 class
    
    * vector3_unit_tests: Add the out operator tests and reorder the tests
    
    * vector3_unit_tests: Fix typos.
    
    * test_vector3: Add tests for the brackets operator
    
    * vector3_unit_tests: Add parameterized tests
    
    * vector3_unit_tests: Use vector with different coordinates.
    
    * vector3_unit_tests: Preface parameterized test name with unit of work.
    
    * vector3_unit_tests: Assign parameter to index.
    
    * vector3_unit_tests: Get expected value, then actual value.
    
    * vector3_unit_tests: Fix the bracket position on some of the tests
    
    * vector3_unit_tests: Renamed the vector used for the parameterized tests
    
    * vector3_unit_tests: Replaced INSTANTIATE_TEST_CASE_P to INSTANTIATE_TEST_SUITE_P to fix deprecation message
    
    Co-authored-by: MooreMachine <8643188+MooreMachine@users.noreply.github.com>

---
+++ code/tests/unittests/test_vector3.cpp
@@ -0,0 +1,172 @@
+#include "vector3.h"
+
+#include "gmock/gmock.h"
+
+#include <tuple>
+
+using namespace testing;
+
+class Vector3Test : public ::testing::Test {
+protected:
+    Vector3 test_vector;
+
+    void SetUp() override {
+        test_vector = Vector3(1.0, 1.0, 1.0);
+    }
+};
+
+class Vector3ParameterizedTest : public ::testing::TestWithParam<std::tuple<int, double>> {
+protected:
+    Vector3 param_test_vector {4.0, 5.0, 6.0 } ;
+};
+
+TEST_F(Vector3Test, OutOperatorWhenPassingAStreamAndVector3OutputsTheVector3Content) {
+    std::string expected_value = "1 1 1";
+    std::stringstream actual_stream;
+
+    actual_stream << test_vector;
+
+    ASSERT_EQ(actual_stream.str(), expected_value);
+}
+
+TEST_F(Vector3Test, PlusOperatorWhenAddingUpTwoVector3ReturnsTheSumOfBothValues) {
+    Vector3 expected_vector(2.0, 2.0, 2.0);
+
+    Vector3 actualVector = test_vector + test_vector;
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, PlusEqualOperatorWhenAddingUpAVector3ReturnsTheSumOfBothValues) {
+    Vector3 expected_vector(2.0, 2.0, 2.0);
+    Vector3 actual_vector = test_vector;
+
+    actual_vector += test_vector;
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, MinusOperatorWhenSubtractingTwoVector3ReturnsTheSubtractionOfBothValues) {
+    Vector3 expected_vector(0.0, 0.0, 0.0);
+
+    Vector3 actualVector = test_vector-test_vector;
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, MinusOperatorWhenNoParamsReturnsInvertedVector3) {
+    Vector3 expected_vector(-1.0, -1.0, -1.0);
+
+    Vector3 actualVector = -test_vector;
+
+    ASSERT_THAT(actualVector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, MultiplyOperatorWhenMultiplyingTwoVector3ReturnsMultiplicationOfBothValues) {
+    Vector3 expected_vector(2.0, 2.0, 2.0);
+
+    Vector3 actual_vector = test_vector * Vector3(2.0, 2.0, 2.0);
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, MultiplyOperatorWhenMultiplyingADoubleAndVector3ReturnsMultiplicationOfBothValues) {
+    Vector3 expected_vector(2.0, 2.0, 2.0);
+
+    Vector3 actual_vector = 2.0 * test_vector;
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, MultiplyEqualOperatorWhenMultiplyingADoubleReturnsMultiplicationOfBothValues) {
+    Vector3 expected_vector(2.0, 2.0, 2.0);
+    Vector3 actual_vector = test_vector;
+
+    actual_vector *= 2.0;
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, DivideOperatorWhenDividingAVector3AndDoubleReturnsDivisionOfBothValues) {
+    Vector3 expected_vector(0.5, 0.5, 0.5);
+
+    Vector3 actual_vector = test_vector / 2.0;
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, DivideEqualsOperatorWhenDividingADoubleReturnsDivisionOfBothValues) {
+    Vector3 expected_vector(0.5, 0.5, 0.5);
+    Vector3 actual_vector = test_vector;
+
+    actual_vector /= 2.0;
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, DotWhenPassingTwoVector3ReturnsDotProductOfBothVectors) {
+    double expectedValue = 3.0;
+
+    double actualValue = Dot(test_vector, test_vector);
+
+    ASSERT_THAT(actualValue, expectedValue);
+}
+
+TEST_F(Vector3Test, CrossWhenPassingTwoVector3ReturnsCrossProductOfBothVectors) {
+    Vector3 expected_vector(0.0, 0.0, 1.0);
+
+    Vector3 actual_vector = Cross(Vector3(1.0, 0.0, 0.0), Vector3(0.0, 1.0, 0.0));
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, UnitVectorWhenPassingAVector3ReturnsItsUnitVector) {
+    Vector3 expected_vector(0.0, 1.0, 0.0);
+
+    Vector3 actual_vector = UnitVector(Vector3(0.0, 1.0, 0.0));
+
+    ASSERT_THAT(actual_vector.e, ElementsAreArray(expected_vector.e));
+}
+
+TEST_F(Vector3Test, LengthWhenNoParamsReturnsSquareRootOfTheVectorsLengthSquared) {
+    double expectedValue = std::sqrt(3.0);
+
+    double actualValue = test_vector.Length();
+
+    ASSERT_THAT(actualValue, expectedValue);
+}
+
+TEST_F(Vector3Test, LengthSquaredWhenNoParamsReturnsLengthSquaredValueOfTheVector) {
+    double expectedValue = 3.0;
+
+    double actualValue = test_vector.LengthSquared();
+
+    ASSERT_THAT(actualValue, expectedValue);
+}
+
+TEST_P(Vector3ParameterizedTest, BracketOperatorWhenPassingAnIndexReturnsDoubleInThatPosition) {
+    int index = std::get<0>(GetParam());
+    double expected_value = std::get<1>(GetParam());
+
+    double actual_value = param_test_vector[index];
+
+    ASSERT_EQ(actual_value, expected_value);
+}
+
+TEST_P(Vector3ParameterizedTest, BracketOperatorWhenPassingAnIndexReturnsDoubleReferenceInThatPosition) {
+    int index = std::get<0>(GetParam());
+    double* expected_value = std::addressof(param_test_vector.e[index]);
+
+    double* actual_value = std::addressof(param_test_vector[index]);
+
+    ASSERT_EQ(actual_value, expected_value);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Vector3BracketOperators,
+    Vector3ParameterizedTest,
+    ::testing::Values(
+        std::make_tuple(0, 4.0), // first -> index, second -> value
+        std::make_tuple(1, 5.0),
+        std::make_tuple(2, 6.0)
+    ));
